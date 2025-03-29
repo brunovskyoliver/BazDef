91ea772a17ae4e71dfb4790c8b9dda5f62400745
olinko_1 <lukaskocman2@gmail.com>
2025-03-29 07:55:45 +0100

    mozme pridat viac metodov

--- Assets/Scripts/gameloop.cs
+++ Assets/Scripts/gameloop.cs
@@ -1,8 +1,45 @@
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class gameloop : MonoBehaviour
 {
-void Start()
-{
-Debug.Log("Game Started");}
+    public Sprite walkerSkin;
+
+    int spawnedmetys = 0;
+    const int numOfMetods = 5;
+    const int timeDelay = 1500; // v milisekundach
+    bool spawnMety = true;
+
+    Stopwatch timer = new Stopwatch();
+    
+    void Start()
+    {
+        UnityEngine.Debug.Log("Game Started");
+
+        timer.Start();
+    }
+
+    void Update()
+    {
+        if (spawnMety)
+        {
+            if (timer.ElapsedMilliseconds > timeDelay)
+            {
+                timer.Restart();
+                GameObject newWalker = new GameObject("Mety");
+                SpriteRenderer renderer = newWalker.AddComponent<SpriteRenderer>();
+                renderer.sprite = walkerSkin;
+                newWalker.AddComponent<Walker>();
+
+                if (spawnedmetys >= numOfMetods) spawnMety = false;
+                spawnedmetys ++;
+
+            }
+        }
+    }
+
+
+
 }
