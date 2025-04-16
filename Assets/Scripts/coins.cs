8315608dff38ef0947987eb1f5fc7cbcd0eaa166
olinko_1 <lukaskocman2@gmail.com>
2025-04-16 13:05:33 +0200

    coins fly

--- Assets/Scripts/coins.cs
+++ Assets/Scripts/coins.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+
 
 class Coins : MonoBehaviour
 {
-    public GameObject coinObject;
+    public Sprite coinSprite;
     public RectTransform coinBag;
     public Camera mainCamera;
+    private gameloop gameloopInstance;
 
     private List<GameObject> allCoins;
     private float xDist;
@@ -15,6 +17,10 @@ class Coins : MonoBehaviour
     private float dist;
     public float speed = 0.01f;
 
+    void Start()
+    {
+        gameloopInstance = FindFirstObjectByType<gameloop>();
+    }
     void Update()
     {
         allCoins = FindGameObjectsInLayer(10);
@@ -28,12 +34,13 @@ class Coins : MonoBehaviour
             yDist = worldPos.y - coin.transform.position.y;
             dist = Vector2.Distance(worldPos, coin.transform.position);
 
-            if (dist < 0.3f) 
+            if (dist < 0.7f) 
             {
                 Destroy(coin);
+                gameloopInstance.money +=1;
                 return;
             }
-            
+
             dist *= dist;
             coin.transform.position = coin.transform.position + new Vector3(xDist/dist * speed, yDist/dist * speed, 0);
 
@@ -56,5 +63,31 @@ class Coins : MonoBehaviour
 
         return result;
     }
+
+    public void SpawnCoins(int n, Vector3 position)
+    {
+        StartCoroutine(Spawn(n, position));
+
+    }
+
+    private IEnumerator Spawn(int n, Vector3 position)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            GameObject newCoin = new GameObject("Coin");
+            newCoin.transform.position = position;
+            newCoin.layer = 10;
+            
+
+            SpriteRenderer sr = newCoin.AddComponent<SpriteRenderer>();
+            sr.sprite = coinSprite;
+            sr.sortingOrder = 500;
+
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+
+  
+
 
 }
